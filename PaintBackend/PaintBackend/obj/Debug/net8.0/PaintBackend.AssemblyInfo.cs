36e51b17fee2e206337c01e66b379454777ef2e5
Kobe Taylor <ktaylor2@linfield.edu>
2025-05-30 12:55:15 -0700

    Updated drawing logic and UI for Paint App

--- PaintBackend/PaintBackend/obj/Debug/net8.0/PaintBackend.AssemblyInfo.cs
+++ PaintBackend/PaintBackend/obj/Debug/net8.0/PaintBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaintBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e173ecf3840686db61261118252b383e6fd7b4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaintBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaintBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
